da11c2b6679b126f6b8b26ceac90759e37d37cae
Matheus-Berlofa <matheus.berlofa@hotmail.com>
2025-11-24 16:09:02 -0300

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -5,12 +5,11 @@ import {
   deleteDoc,
   doc
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-
 import {
   onAuthStateChanged
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
-const listaDiv = document.getElementById("listaReservas");
+const lista = document.getElementById("listaReservas");
 
 onAuthStateChanged(auth, async (user) => {
   if (!user) {
@@ -22,42 +21,42 @@ onAuthStateChanged(auth, async (user) => {
 });
 
 async function carregarReservas() {
-  listaDiv.innerHTML = "<p>Carregando reservas...</p>";
+  lista.innerHTML = "Carregando reservas...";
 
-  const querySnapshot = await getDocs(collection(db, "reservas"));
+  const snap = await getDocs(collection(db, "reservas"));
 
-  if (querySnapshot.empty) {
-    listaDiv.innerHTML = "<p>Nenhuma reserva encontrada.</p>";
+  if (snap.empty) {
+    lista.innerHTML = "<p>Nenhuma reserva encontrada.</p>";
     return;
   }
 
-  listaDiv.innerHTML = "";
+  lista.innerHTML = "";
 
-  querySnapshot.forEach((docSnap) => {
-    const dados = docSnap.data();
+  snap.forEach((reserva) => {
+    const r = reserva.data();
 
     const card = document.createElement("div");
     card.classList.add("card");
 
     card.innerHTML = `
-      <strong>Nome:</strong> ${dados.nome} <br>
-      <strong>Email:</strong> ${dados.email} <br>
-      <strong>Data:</strong> ${dados.data} <br>
-      <strong>Horário:</strong> ${dados.horario} <br>
-      <strong>Mesa:</strong> ${dados.mesa} <br>
-      <strong>Quantidade:</strong> ${dados.quantidade} <br>
-      <button class="btnExcluir" data-id="${docSnap.id}">Excluir</button>
+      <strong>Nome:</strong> ${r.nome}<br>
+      <strong>Email:</strong> ${r.email}<br>
+      <strong>Data:</strong> ${r.data}<br>
+      <strong>Horário:</strong> ${r.horario}<br>
+      <strong>Mesa:</strong> ${r.mesa}<br>
+      <strong>Quantidade:</strong> ${r.quantidade}<br>
+      <button class="btnExcluir" data-id="${reserva.id}">Excluir</button>
     `;
 
-    listaDiv.appendChild(card);
+    lista.appendChild(card);
   });
 
-  document.querySelectorAll(".btnExcluir").forEach((btn) => {
+  document.querySelectorAll(".btnExcluir").forEach(btn => {
     btn.addEventListener("click", async () => {
       const id = btn.getAttribute("data-id");
       await deleteDoc(doc(db, "reservas", id));
       alert("Reserva excluída!");
-      carregarReservas(); // recarrega
+      carregarReservas();
     });
   });
 }
