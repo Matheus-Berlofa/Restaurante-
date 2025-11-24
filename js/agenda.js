e27ce5d75d13464fde554a16ab2eb500965e1901
Matheus-Berlofa <matheus.berlofa@hotmail.com>
2025-11-24 14:48:39 -0300

    Update agenda.js

--- js/agenda.js
+++ js/agenda.js
@@ -87,7 +87,7 @@ document.getElementById("agendaForm").addEventListener("submit", async (e) => {
       data,
       horario: horarioSelecionado,
       mesa: mesaSelecionada,
-      quantidade: qtd,
+      quantidade_de_pessoas: qtd,
       criadoEm: new Date(),
       nome: nomeUsuarioLogado,
       email: emailUsuarioLogado,
@@ -107,3 +107,4 @@ document.getElementById("agendaForm").addEventListener("submit", async (e) => {
     mensagem.style.color = "red";
   }
 });
+
