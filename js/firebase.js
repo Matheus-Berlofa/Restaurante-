e7d345df39500febc73f1eaeba63e5593c02f25b
Matheus-Berlofa <matheus.berlofa@hotmail.com>
2025-11-24 12:29:29 -0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,14 +1,15 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
-  authDomain: "SEU_PROJETO.firebaseapp.com",
-  projectId: "SEU_PROJETO",
-  storageBucket: "SEU_PROJETO.appspot.com",
-  messagingSenderId: "XXXXXXX",
-  appId: "XXXXXXX"
+  apiKey: "AIzaSyAoNt8GLu-6kg8iezNW45UMxVKNUH1y3KM",
+  authDomain: "restaurante-b7b3b.firebaseapp.com",
+  projectId: "restaurante-b7b3b",
+  storageBucket: "restaurante-b7b3b.firebasestorage.app",
+  messagingSenderId: "736095610672",
+  appId: "1:736095610672:web:bf1e7e27653a0b2cd24af5",
+  measurementId: "G-E96TVPVD1C"
 };
 
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
