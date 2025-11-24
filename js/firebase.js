43e1a3621fbebc0d663e849f1a139d8674aeef5a
Matheus-Berlofa <matheus.berlofa@hotmail.com>
2025-11-24 12:27:49 -0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,17 +1,14 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAoNt8GLu-6kg8iezNW45UMxVKNUH1y3KM",
-  authDomain: "restaurante-b7b3b.firebaseapp.com",
-  projectId: "restaurante-b7b3b",
-  storageBucket: "restaurante-b7b3b.firebasestorage.app",
-  messagingSenderId: "736095610672",
-  appId: "1:736095610672:web:bf1e7e27653a0b2cd24af5",
-  measurementId: "G-E96TVPVD1C"
+  apiKey: "SUA_API_KEY",
+  authDomain: "SEU_PROJETO.firebaseapp.com",
+  projectId: "SEU_PROJETO",
+  storageBucket: "SEU_PROJETO.appspot.com",
+  messagingSenderId: "XXXXXXX",
+  appId: "XXXXXXX"
 };
 
-export const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
